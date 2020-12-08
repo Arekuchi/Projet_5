59e0957d6c94f64a04d2ce0c12533699864da8d2
Alexis Berthelot <berthelota2@gmail.com>
2020-12-08 18:11:03 +0100

    ajout exception et update datarepo

--- src/main/java/com/openclassrooms/safetyAlerts/repository/DataRepository.java
+++ src/main/java/com/openclassrooms/safetyAlerts/repository/DataRepository.java
@@ -5,10 +5,13 @@ import com.openclassrooms.safetyAlerts.dao.Database;
 import com.openclassrooms.safetyAlerts.dao.Firestation;
 import com.openclassrooms.safetyAlerts.dao.Medicalrecord;
 import com.openclassrooms.safetyAlerts.dao.Person;
+import com.openclassrooms.safetyAlerts.exceptions.DataRepositoryException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Repository;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -17,8 +20,13 @@ import java.util.List;
 public class DataRepository {
     // Permets de mapper du json en objet java
     private static final ObjectMapper objectMapper = new ObjectMapper();
+    // C'est pour le LOG4J
+    private static final Logger logger = LogManager.getLogger(DataRepository.class);
     // Cet objet va representer le json en mémoire JAVA
     private static Database database;
+    // Boolean pour valider le commit (main OK, test NOT OK)
+    private boolean commit = true;
+    private String DATA_JSON = "data.json";
 
     public Database getDatabase() {
         return this.database;
@@ -26,8 +34,41 @@ public class DataRepository {
 
     // Utilise les déclarations ci dessus pour mapper l'objet.
     public DataRepository() throws IOException {
-        InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("data.json");
-        database = objectMapper.readerFor(Database.class).readValue(inputStream);
+        this.init();
+    }
+
+    public void init() {
+        try (InputStream ips = ClassLoader.getSystemResourceAsStream(DATA_JSON)) {
+            database = objectMapper.readerFor(Database.class).readValue(ips);
+            logger.info("OK - File_OPEN : " + DATA_JSON);
+        } catch (FileNotFoundException fnfe) {
+            logger.info("KO - File_NOT_FOUND : " + DATA_JSON);
+            throw new DataRepositoryException("KO - File_NOT_FOUND : ", fnfe);
+        } catch (IOException ioe) {
+            logger.info("KO - I/O ERROR : " + DATA_JSON);
+            throw new DataRepositoryException("KO - I/O ERROR ", ioe);
+        }
+    }
+
+    public void commit() {
+        // récup le path du JSON
+        URL url = ClassLoader.getSystemResource(DATA_JSON);
+        // on ouvre un flux d'écriture vers le JSON
+        try (OutputStream ops = new FileOutputStream(url.getFile())) {
+            // ecrire le fichier JSON avec un formatage
+            objectMapper.writerWithDefaultPrettyPrinter().writeValue(ops, database);
+            logger.info("OK - File_UPDATED : " + DATA_JSON);
+        } catch (FileNotFoundException fnfe) {
+            logger.info("KO - File_NOT_FOUND : " + DATA_JSON);
+            throw new DataRepositoryException("KO - File_NOT_FOUND : ", fnfe);
+        } catch (IOException ioe) {
+            logger.info("KO - I/O ERROR : " + DATA_JSON);
+            throw new DataRepositoryException("KO - I/O ERROR ", ioe);
+        }
+    }
+
+    public void setCommit(boolean commit) {
+        this.commit = commit;
     }
 
     //Méthode pour récupéré les persons du data.json
